f7cafec5a1ca73d43a6cbed086cf5f687646b164
shidoitsuka <shidoitsukaa@gmail.com>
2019-10-07 21:25:46 +0700

    first initial

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require("electron");
+let wind;
+
+const loadAppWindow = () => {
+  wind = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  wind.loadFile("./views/index.html");
+};
+
+app.on("ready", loadAppWindow);
